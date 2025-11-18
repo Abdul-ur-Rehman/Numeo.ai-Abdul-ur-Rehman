d09258587e1359c0cc08b5a26aef495d6089d243
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-11-18 13:55:00 +0500

    driver factory fixes for CI flow

--- utilities/driver_factory.py
+++ utilities/driver_factory.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
-
 from selenium.webdriver.chrome.service import Service
 
 def create_driver():
     options = webdriver.ChromeOptions()
-    options.add_argument('--start-maximized')
-    options.add_argument("--headless")
-    return webdriver.Chrome(options=options)
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
+
+    service = Service()
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
