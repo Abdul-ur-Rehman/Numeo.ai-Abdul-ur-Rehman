2c3b06fd972f7e9d7da2c058c94e41f41ad00731
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-11-18 13:23:40 +0500

    driver factory --headless

--- utilities/driver_factory.py
+++ utilities/driver_factory.py
@@ -5,5 +5,5 @@ from selenium.webdriver.chrome.service import Service
 def create_driver():
     options = webdriver.ChromeOptions()
     options.add_argument('--start-maximized')
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     return webdriver.Chrome(options=options)
