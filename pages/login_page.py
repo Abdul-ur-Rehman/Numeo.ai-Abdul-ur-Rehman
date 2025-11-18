2c5b0597669e5389eeaa6d81938d44c6a0c2198c
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-11-18 13:11:57 +0500

    login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,16 +1,22 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
-    def __init__(self, driver):
+    URL = "https://www.saucedemo.com/"
+
+    def __init__(self, driver, timeout=10):
         self.driver = driver
+        self.wait = WebDriverWait(driver, timeout)
+
         self.username = (By.ID, "user-name")
         self.password = (By.ID, "password")
         self.login_btn = (By.ID, "login-button")
 
     def load(self):
-        self.driver.get("https://www.saucedemo.com")
+        self.driver.get(self.URL)
 
     def login(self, user, pwd):
-        self.driver.find_element(*self.username).send_keys(user)
+        self.wait.until(EC.visibility_of_element_located(self.username)).send_keys(user)
         self.driver.find_element(*self.password).send_keys(pwd)
         self.driver.find_element(*self.login_btn).click()
