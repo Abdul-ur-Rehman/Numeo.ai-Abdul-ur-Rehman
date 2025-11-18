c935ec429f58611fd5ea3db73cfdc5eb07641fe8
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-11-18 19:48:35 +0500

    conftest updated

--- conftest.py
+++ conftest.py
@@ -10,26 +10,32 @@ def driver():
 
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item):
-    """
-        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
-        :param item:
-        """
-    pytest_html = item.config.pluginmanager.getplugin('html')
+    pytest_html = item.config.pluginmanager.getplugin("html")
     outcome = yield
     report = outcome.get_result()
-    extra = getattr(report, 'extra', [])
+    extra = getattr(report, "extra", [])
 
-    if report.when == 'call' or report.when == "setup":
-        xfail = hasattr(report, 'wasxfail')
+    if report.when in ("setup", "call"):
+        xfail = hasattr(report, "wasxfail")
+
         if (report.skipped and xfail) or (report.failed and not xfail):
-            file_name = report.nodeid.replace("::", "_") + ".png"
-            _capture_screenshot(file_name)
-            if file_name:
-                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
-                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+            driver = item.funcargs.get("driver", None)
+
+            if driver and hasattr(driver, "get_screenshot_as_file"):
+
+                file_name = report.nodeid.replace("::", "_") + ".png"
+                _capture_screenshot(driver, file_name)
+
+                html = (
+                    f'<div><img src="{file_name}" alt="screenshot" '
+                    f'style="width:304px;height:228px;" '
+                    f'onclick="window.open(this.src)" align="right"/></div>'
+                )
                 extra.append(pytest_html.extras.html(html))
+
         report.extra = extra
 
 
-def _capture_screenshot(name):
-        driver.get_screenshot_as_file(name)
+def _capture_screenshot(driver, name):
+    driver.get_screenshot_as_file(name)
+
